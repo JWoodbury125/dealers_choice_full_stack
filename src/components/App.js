273518da1aa218a84ca8112c9fc8d95d4884242d
Jennifer Woodbury <jenann125@yahoo.com>
2022-03-19 19:17:43 -0400

    Incremental Updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React from "react";
-// import Members from "./Members";
+import Members from "./Members";
 // import store from "../store";
+import axios from "axios";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      loading: false,
+      members: [],
+      error: "",
+    };
+  }
+  async componentDidMount() {
+    const response = await axios.get("/members");
+    this.setState({ members: response.data });
+    console.log(this.state.members);
   }
 
   render() {
     return (
       <ul>
-        <hr />
+        <Members members={this.state.members} />
       </ul>
     );
   }
--- src/components/Members.js
+++ src/components/Members.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Member } from "./Member";
+// import { Member } from "./Member";
 
 const Members = (props) => {
   return (
     <ul>
       {props.members.map((member) => {
-        <Member key={member.id} />;
+        return (
+          <div key={member.id}>
+            <a href="/">
+              {member.firstName} {member.lastName}
+            </a>
+            <button onClick={() => console.log("button works")}>x</button>
+          </div>
+        );
       })}
     </ul>
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Members);
+export default Members;
