7e284b055f47f1b63c6474697bbd2be3898244cd
Jennifer Woodbury <jenann125@yahoo.com>
2022-03-19 11:31:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const { db, Member, Dues, syncAndSeed } = require("./db");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get("/members", async (req, res, next) => {
+  try {
+    const members = await Member.findAll();
+    res.send(members);
+  } catch (err) {
+    next(err);
+  }
+});
+app.get("/members/:memberId", async (req, res, next) => {
+  try {
+    const member = await Member.findByPk(req.params.memberId);
+    res.send(member);
+  } catch (err) {
+    next(err);
+  }
+});
+
+const init = async () => {
+  try {
+    await db.sync({ force: true });
+    await syncAndSeed();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
