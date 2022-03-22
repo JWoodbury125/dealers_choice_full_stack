fb669698d5d24e24e7d9de5e39bd045515ec2b53
Jennifer Woodbury <jenann125@yahoo.com>
2022-03-22 18:39:16 -0400

    Heroku deploy

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import {
-  fetchMembers,
-  addMember,
-  deleteMember,
-  fetchMemberDetail,
-} from "../store";
+import { fetchMembers, addMember, deleteMember } from "../store";
 import { Link } from "react-router-dom";
 
 class Members extends React.Component {
